8cd1c8a7c4e52ac72f1ff4c688bdf4ceab0c9fa2
AvinashPandey2000 <avinash201pandey@gmail.com>
2023-01-20 10:35:14 +0530

    com changes

---
+++ src/module1.mjs
@@ -0,0 +1,9 @@
+const a="avinash"
+const n="nishant";
+const l="lokesh";
+
+
+
+export default a;
+export {n};
+export {l};
---
+++ src/module2.mjs
@@ -0,0 +1,5 @@
+import name,{n,l} from './module1.mjs';
+
+console.log(name);
+
+console.log(n)
